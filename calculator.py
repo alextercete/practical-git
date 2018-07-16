37ec8b912ddcb90b63e5d1d76c6a7e88c39970b2
Alex Tercete <alextercete@gmail.com>
2018-07-16 22:23:58 +0100

    Implement division

--- calculator.py
+++ calculator.py
@@ -7,3 +7,8 @@ class Calculator:
 
     def multiply(self, firstNumber, secondNumber):
         return firstNumber * secondNumber
+
+    def divide(self, firstNumber, secondNumber):
+        if secondNumber == 0:
+            raise ValueError('Cannot divide by zero!')
+        return firstNumber / secondNumber
