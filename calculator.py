941f24f2d610c4ff30b18719dd78e2f79d0ae302
Alex Tercete <alextercete@gmail.com>
2018-07-16 22:22:32 +0100

    Rename parameters

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def add(self, firstNumber, secondNumber):
+        return firstNumber + secondNumber
 
-    def subtract(self, a, b):
-        return a - b
+    def subtract(self, firstNumber, secondNumber):
+        return firstNumber - secondNumber
