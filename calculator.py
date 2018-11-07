01a67cce194af7a93d0be40937eee9ac5f3799d5
Alex Tercete <alextercete@gmail.com>
2018-11-07 08:47:26 -0500

    Initialise SUT in setUp method
    Oops, this commit includes some unrelated changes.

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 class Calculator:
     def add(self, operand1, operand2):
+        print('Called add')
         return operand1 + operand2
 
     def subtract(self, operand1, operand2):
@@ -9,4 +10,5 @@ class Calculator:
         return operand1 * operand2
 
     def divide(self, operand1, operand2):
+        # TODO: raise ValueError when operand2 is zero
         return operand1 / operand2
