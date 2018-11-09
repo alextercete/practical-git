ccd4061623b1db98d27ea604597508b6a3e43f9b
Alex Tercete <alextercete@gmail.com>
2018-11-08 19:19:51 -0500

    Implement multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -10,5 +10,9 @@ class CalculatorTests(unittest.TestCase):
         calculator = Calculator()
         self.assertEqual(3, calculator.subtract(5, 2))
 
+    def test_multiplication(self):
+        calculator = Calculator()
+        self.assertEqual(6, calculator.multiply(3, 2))
+
 if __name__ == '__main__':
     unittest.main()
